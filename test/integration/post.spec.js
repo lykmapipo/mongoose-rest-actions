6ba006c592279f1e8405de31fdc4f0c41fc11477
lykmapipo <lallyelias87@gmail.com>
2019-04-21 01:11:01 +0300

    refactor(post): use instance save & improve specs

--- test/integration/post.spec.js
+++ test/integration/post.spec.js
@@ -1,17 +1,19 @@
 'use strict';
 
 /* dependencies */
-const async = require('async');
+const _ = require('lodash');
+const { waterfall } = require('async');
 const { expect } = require('chai');
 const { include } = require('@lykmapipo/include');
 const { ObjectId } = require('@lykmapipo/mongoose-common');
 const {
+  create,
   clear,
   createTestModel
 } = require('@lykmapipo/mongoose-test-helpers');
 const actions = include(__dirname, '..', '..');
 
-describe.only('post', () => {
+describe('post', () => {
 
   const Guardian = createTestModel({
     email: { type: String, unique: true, fake: f => f.internet.email() }
@@ -19,85 +21,135 @@ describe.only('post', () => {
 
   const Child = createTestModel({
     email: { type: String, unique: true, fake: f => f.internet.email() },
-    mother: {
-      type: ObjectId,
-      ref: Guardian.modelName,
-      fake: () => Guardian.fake()
-    }
+    father: { type: ObjectId, ref: Guardian.modelName }
   }, actions);
+
+  let father = Guardian.fake();
 
   before(done => clear(Guardian, Child, done));
 
+  before(done => create(father, done));
+
   it('should work using `post` static method', done => {
-    const father = Guardian.fake();
-    Guardian.post(father, (error, created) => {
+    const guardian = Guardian.fake();
+    Guardian.post(guardian.toObject(), (error, created) => {
       expect(error).to.not.exist;
       expect(created).to.exist;
-      expect(created._id).to.exist;
-      expect(created.name).to.equal(father.name);
-      expect(created.email).to.equal(father.email);
+      expect(created._id).to.exist.and.be.eql(guardian._id);
+      expect(created.name).to.equal(guardian.name);
+      expect(created.email).to.equal(guardian.email);
+      expect(created.createdAt).to.exist;
+      expect(created.updatedAt).to.exist;
       done(error, created);
     });
   });
 
-  it.skip('should work using `post` static method', done => {
+  it('should work with instance using `post` static method', done => {
+    const guardian = Guardian.fake();
+    Guardian.post(guardian, (error, created) => {
+      expect(error).to.not.exist;
+      expect(created).to.exist;
+      expect(created._id).to.exist.and.be.eql(guardian._id);
+      expect(created.name).to.equal(guardian.name);
+      expect(created.email).to.equal(guardian.email);
+      expect(created.createdAt).to.exist;
+      expect(created.updatedAt).to.exist;
+      done(error, created);
+    });
+  });
+
+  it('should work with refs using `post` static method', done => {
     const child = Child.fake();
+    child.father = father;
     Child.post(child, (error, created) => {
       expect(error).to.not.exist;
       expect(created).to.exist;
-      expect(created._id).to.exist;
+      expect(created._id).to.exist.and.be.eql(child._id);
       expect(created.name).to.equal(child.name);
-      expect(created.age).to.equal(child.age);
-      expect(created.year).to.equal(child.year);
+      expect(created.email).to.equal(child.email);
+      expect(created.createdAt).to.exist;
+      expect(created.updatedAt).to.exist;
+      expect(created.father).to.exist;
+      expect(created.father._id).to.exist.and.be.eql(father._id);
+      expect(created.father.name).to.equal(father.name);
+      expect(created.father.email).to.equal(father.email);
       done(error, created);
     });
   });
 
-  it.skip('should be able to post instance', done => {
-    const father = Guardian.fake();
-
-    Guardian.post(father, (error, created) => {
+  it('should work using `post` instance method', done => {
+    const guardian = Guardian.fake();
+    guardian.post((error, created) => {
       expect(error).to.not.exist;
       expect(created).to.exist;
-      expect(created._id).to.exist;
-      expect(created.name).to.equal(father.name);
-      expect(created.age).to.equal(father.age);
+      expect(created._id).to.exist.and.be.eql(guardian._id);
+      expect(created.name).to.equal(guardian.name);
+      expect(created.email).to.equal(guardian.email);
+      expect(created.createdAt).to.exist;
+      expect(created.updatedAt).to.exist;
       done(error, created);
     });
   });
 
-  it.skip('should beautify unique error message', done => {
-    const father = Guardian.fake();
+  it('should work with refs using `post` instance method', done => {
+    const child = Child.fake();
+    child.father = father;
+    child.post((error, created) => {
+      expect(error).to.not.exist;
+      expect(created).to.exist;
+      expect(created._id).to.exist.and.be.eql(child._id);
+      expect(created.name).to.equal(child.name);
+      expect(created.email).to.equal(child.email);
+      expect(created.createdAt).to.exist;
+      expect(created.updatedAt).to.exist;
+      expect(created.father).to.exist;
+      expect(created.father._id).to.exist.and.be.eql(father._id);
+      expect(created.father.name).to.equal(father.name);
+      expect(created.father.email).to.equal(father.email);
+      done(error, created);
+    });
+  });
 
-    // wait index
-    // Guardian.on('index', () => {
+  it('should beautify unique error using `post` static method', done => {
+    const guardian = _.pick(Guardian.fake(), 'name', 'email');
 
-    async.waterfall([
-      //...take 1
-      (next) => {
-        Guardian.post(father, next);
-      },
-
-      //...take 2
-      (saved, next) => {
-        Guardian.post(father, next);
-      }
-    ], (error, result) => {
+    waterfall([
+      next => Guardian.post(guardian, error => next(error)),
+      next => Guardian.post(guardian, error => next(error)),
+    ], error => {
       expect(error).to.exist;
       expect(error.status).to.exist;
       expect(error.name).to.exist;
       expect(error.name).to.be.equal('ValidationError');
-      expect(error._message).to.exist;
+      expect(error.message).to.exist;
       expect(error.errors).to.exist;
-      expect(error.errors.name).to.exist;
-      expect(error.errors.name.kind).to.exist;
-      expect(error.errors.name.kind).to.be.equal('unique');
-      done(null, result);
-
+      expect(error.errors.email).to.exist;
+      expect(error.errors.email.kind).to.exist;
+      expect(error.errors.email.kind).to.be.equal('unique');
+      expect(error.errors.email.value).to.be.equal(guardian.email);
+      done();
     });
+  });
 
-    // });
+  it('should beautify unique error using `post` instance method', done => {
+    const guardian = _.pick(Guardian.fake(), 'name', 'email');
 
+    waterfall([
+      next => new Guardian(guardian).post(error => next(error)),
+      next => new Guardian(guardian).post(error => next(error)),
+    ], error => {
+      expect(error).to.exist;
+      expect(error.status).to.exist;
+      expect(error.name).to.exist;
+      expect(error.name).to.be.equal('ValidationError');
+      expect(error.message).to.exist;
+      expect(error.errors).to.exist;
+      expect(error.errors.email).to.exist;
+      expect(error.errors.email.kind).to.exist;
+      expect(error.errors.email.kind).to.be.equal('unique');
+      expect(error.errors.email.value).to.be.equal(guardian.email);
+      done();
+    });
   });
 
   before(done => clear(Guardian, Child, done));
