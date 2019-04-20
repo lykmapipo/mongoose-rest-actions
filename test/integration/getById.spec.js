cc8cda762b3baf08c9e701ae0d34aa57cd8a11e0
lykmapipo <lallyelias87@gmail.com>
2019-04-21 02:52:30 +0300

    test(integration): refactor & improve getById specs

--- test/integration/getById.spec.js
+++ test/integration/getById.spec.js
@@ -1,112 +1,97 @@
 'use strict';
 
-//dependencies
-const path = require('path');
-const _ = require('lodash');
-const async = require('async');
-const faker = require('faker');
-const chai = require('chai');
-const mongoose = require('mongoose');
-const { model } = require('@lykmapipo/mongoose-common');
-const Schema = mongoose.Schema;
-const ObjectId = Schema.Types.ObjectId;
-const expect = chai.expect;
-const actions = require(path.join(__dirname, '..', '..'));
+/* dependencies */
+// const _ = require('lodash');
+const { expect } = require('chai');
+const { include } = require('@lykmapipo/include');
+const { ObjectId } = require('@lykmapipo/mongoose-common');
+const {
+  create,
+  clear,
+  createTestModel
+} = require('@lykmapipo/mongoose-test-helpers');
+const actions = include(__dirname, '..', '..');
 
-describe('integration#getById', () => {
+describe('getById', () => {
 
+  const Guardian = createTestModel({
+    email: { type: String, unique: true, fake: f => f.internet.email() }
+  }, actions);
 
-  const modelName = 'GetByIdIntegration';
-  const UserSchema = new Schema({
-    name: { type: String, searchable: true, index: true, fake: true },
-    age: { type: Number, index: true },
-    year: { type: Number, index: true },
-    mother: { type: ObjectId, ref: modelName, index: true, autoset: true },
-    father: { type: ObjectId, ref: modelName, index: true, autoset: true }
-  });
-  UserSchema.plugin(actions);
-  const User = model(modelName, UserSchema);
+  const Child = createTestModel({
+    email: { type: String, unique: true, fake: f => f.internet.email() },
+    father: { type: ObjectId, ref: Guardian.modelName }
+  }, actions);
 
-  let father = { name: faker.name.firstName(), age: 58, year: 1960 };
-  let mother = { name: faker.name.firstName(), age: 48, year: 1970 };
-  let kids = _.map(_.range(1, 3), (age) => {
-    return { name: faker.name.firstName(), age: age, year: 1980 + age };
-  });
+  let father = Guardian.fake();
+  let child = Child.fake();
+  child.father = father;
 
-  before((done) => {
-    User.deleteMany(done);
-  });
+  before(done => clear(Guardian, Child, done));
 
-  //seed user
-  before((done) => {
-    async.waterfall([
-      (next) => {
-        async.parallel({
-          mother: (next) => {
-            User.create(mother, next);
-          },
-          father: (next) => {
-            User.create(father, next);
-          }
-        }, next);
-      },
-      (parents, next) => {
-        mother = parents.mother;
-        father = parents.father;
-        User
-          .create(
-            _.map(kids, (kid) => {
-              return _.merge({}, kid, parents);
-            }),
-            (error, created) => {
-              kids = created;
-              next(error, created);
-            });
-      }
-    ], done);
+  before(done => create(father, done));
 
+  before(done => create(child, done));
+
+  it('should work with `ObjectId` using `getById` static method', done => {
+    Guardian.getById(father._id, (error, found) => {
+      expect(error).to.not.exist;
+      expect(found).to.exist;
+      expect(found._id).to.exist.and.be.eql(father._id);
+      expect(found.name).to.equal(father.name);
+      expect(found.email).to.equal(father.email);
+      expect(found.createdAt).to.exist;
+      expect(found.updatedAt).to.exist;
+      done(error, found);
+    });
   });
 
+  it('should work with `ObjectId` using `getById` static method', done => {
+    Child.getById(child._id, (error, found) => {
+      expect(error).to.not.exist;
+      expect(found).to.exist;
+      expect(found._id).to.exist.and.be.eql(child._id);
+      expect(found.name).to.equal(child.name);
+      expect(found.email).to.equal(child.email);
+      expect(found.createdAt).to.exist;
+      expect(found.updatedAt).to.exist;
+      done(error, found);
+    });
+  });
 
-  it('should be able to get by object id', (done) => {
-
-    User
-      .getById(father._id, (error, found) => {
-        expect(error).to.not.exist;
-        expect(found).to.exist;
-        expect(found._id).to.eql(found._id);
-        done(error, found);
+  it('should fail using `getById` static method if instance not exist',
+    done => {
+      const guardian = Guardian.fake();
+      Guardian.getById(guardian._id, error => {
+        expect(error).to.exist;
+        expect(error.message).to.exist.and.be.equal('Not Found');
+        expect(error.status).to.exist.and.be.equal(404);
+        done();
       });
+    });
 
-  });
-
-  it('should be able to get by options', (done) => {
-
-    const kid = kids[0];
+  it('should work with options using `getById` static method', done => {
     const options = {
-      _id: kid._id,
+      _id: child._id,
       select: 'name',
-      populate: [{ path: 'mother', select: 'name' }]
+      populate: [{ path: 'father', select: 'name' }]
     };
-
-    User
-      .getById(options, (error, found) => {
-        expect(error).to.not.exist;
-        expect(found).to.exist;
-        expect(found._id).to.eql(kid._id);
-        expect(found.age).to.not.exist;
-        expect(found.year).to.not.exist;
-        expect(found.father).to.not.exist;
-        expect(found.mother).to.exist;
-        expect(found.mother._id).to.be.eql(mother._id);
-        done(error, found);
-      });
-
+    Child.getById(options, (error, found) => {
+      expect(error).to.not.exist;
+      expect(found).to.exist;
+      expect(found._id).to.exist.and.be.eql(child._id);
+      expect(found.name).to.equal(child.name);
+      expect(found.email).to.not.exist;
+      expect(found.createdAt).not.exist;
+      expect(found.updatedAt).not.exist;
+      expect(found.father).to.exist;
+      expect(found.father._id).to.exist.and.be.eql(father._id);
+      expect(found.father.name).to.exist.and.to.equal(father.name);
+      expect(found.father.email).to.not.exist;
+      done(error, found);
+    });
   });
 
-
-  after((done) => {
-    User.deleteMany(done);
-  });
+  after(done => clear(Guardian, Child, done));
 
 });
