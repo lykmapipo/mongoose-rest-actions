c6bb9056b2431a74db3d87358f4cdaff002692bb
lykmapipo <lallyelias87@gmail.com>
2019-04-21 03:22:28 +0300

    test(integration): refactor & improve get and fresh specs

--- test/integration/get.spec.js
+++ test/integration/get.spec.js
@@ -1,278 +1,205 @@
 'use strict';
 
-//dependencies
-const path = require('path');
+/* dependencies */
 const _ = require('lodash');
-const async = require('async');
-const faker = require('faker');
-const chai = require('chai');
-const mongoose = require('mongoose');
-const { model } = require('@lykmapipo/mongoose-common');
-const Schema = mongoose.Schema;
-const ObjectId = Schema.Types.ObjectId;
-const expect = chai.expect;
-const actions = require(path.join(__dirname, '..', '..'));
+const { expect } = require('chai');
+const { include } = require('@lykmapipo/include');
+const { ObjectId } = require('@lykmapipo/mongoose-common');
+const {
+  create,
+  clear,
+  createTestModel
+} = require('@lykmapipo/mongoose-test-helpers');
+const actions = include(__dirname, '..', '..');
 
-describe('integration#get', () => {
+describe('get', () => {
 
-  const modelName = 'GetableIntegration';
-  const UserSchema = new Schema({
-    name: { type: String, searchable: true, index: true, fake: true },
-    age: { type: Number, index: true },
-    year: { type: Number, index: true },
-    mother: {
-      type: ObjectId,
-      ref: modelName,
-      index: true,
-      autoset: true,
-      autopopulate: true
-    },
-    father: {
-      type: ObjectId,
-      ref: modelName,
-      index: true,
-      autoset: true,
-      autopopulate: true
-    }
-  }, { timestamps: { createdAt: 'getCreatedAt', updatedAt: 'getUpdatedAt' } });
-  UserSchema.plugin(actions);
-  const User = model(modelName, UserSchema);
+  const Guardian = createTestModel({
+    email: { type: String, unique: true, fake: f => f.internet.email() },
+    age: { type: Number, min: 30, fake: true },
+  }, actions);
 
-  const father = { name: faker.name.firstName(), age: 58, year: 1960 };
-  const mother = { name: faker.name.firstName(), age: 48, year: 1970 };
-  const kids = _.map(_.range(1, 31), (age) => {
-    return { name: faker.name.firstName(), age: age, year: 1980 + age };
+  const Child = createTestModel({
+    email: { type: String, unique: true, fake: f => f.internet.email() },
+    age: { type: Number, min: 3, fake: true },
+    father: { type: ObjectId, ref: Guardian.modelName }
+  }, actions);
+
+  let father = Guardian.fake();
+  let children = _.map(Child.fake(32), child => {
+    child.father = father;
+    return child;
   });
 
-  before((done) => {
-    User.deleteMany(done);
+  before(done => clear(Guardian, Child, done));
+
+  before(done => create(father, done));
+
+  before(done => create(...children, done));
+
+  it('should work with no options using `get` static method', done => {
+    Child.get((error, results) => {
+      expect(error).to.not.exist;
+      expect(results).to.exist;
+      expect(results.data).to.exist;
+      expect(results.data).to.have.length(10);
+      expect(results.total).to.exist;
+      expect(results.total).to.be.equal(32);
+      expect(results.limit).to.exist;
+      expect(results.limit).to.be.equal(10);
+      expect(results.skip).to.exist;
+      expect(results.skip).to.be.equal(0);
+      expect(results.page).to.exist;
+      expect(results.page).to.be.equal(1);
+      expect(results.pages).to.exist;
+      expect(results.pages).to.be.equal(4);
+      expect(results.lastModified).to.exist;
+      expect(_.maxBy(results.data, 'updatedAt').updatedAt)
+        .to.be.at.most(results.lastModified);
+      done(error, results);
+    });
   });
 
-  //seed user
-  before((done) => {
-    async.waterfall([
-      (next) => {
-        async.parallel({
-          mother: (next) => {
-            User.create(mother, next);
-          },
-          father: (next) => {
-            User.create(father, next);
-          }
-        }, next);
-      },
-      (parents, next) => {
-        User
-          .create(
-            _.map(kids, (kid) => { return _.merge({}, kid, parents); }),
-            next
-          );
-      }
-    ], done);
-
-  });
-
-
-  it('should be able to get without options', (done) => {
-
-    User
-      .get((error, results) => {
-        expect(error).to.not.exist;
-        expect(results).to.exist;
-        expect(results.data).to.exist;
-        expect(results.data).to.have.length(10);
-        expect(results.total).to.exist;
-        expect(results.total).to.be.equal(32);
-        expect(results.limit).to.exist;
-        expect(results.limit).to.be.equal(10);
-        expect(results.skip).to.exist;
-        expect(results.skip).to.be.equal(0);
-        expect(results.page).to.exist;
-        expect(results.page).to.be.equal(1);
-        expect(results.pages).to.exist;
-        expect(results.pages).to.be.equal(4);
-        expect(results.lastModified).to.exist;
-        expect(_.maxBy(results.data, 'getUpdatedAt').getUpdatedAt)
-          .to.be.at.most(results.lastModified);
-        done(error, results);
-      });
-
-  });
-
-  it('should be able to get with options', (done) => {
-
+  it('should work with page and limit using `get` static method', done => {
     const options = { page: 1, limit: 20 };
-    User
-      .get(options, (error, results) => {
-        expect(error).to.not.exist;
-        expect(results).to.exist;
-        expect(results.data).to.exist;
-        expect(results.data).to.have.length(20);
-        expect(results.total).to.exist;
-        expect(results.total).to.be.equal(32);
-        expect(results.limit).to.exist;
-        expect(results.limit).to.be.equal(20);
-        expect(results.skip).to.exist;
-        expect(results.skip).to.be.equal(0);
-        expect(results.page).to.exist;
-        expect(results.page).to.be.equal(1);
-        expect(results.pages).to.exist;
-        expect(results.pages).to.be.equal(2);
-        expect(results.lastModified).to.exist;
-        expect(_.maxBy(results.data, 'getUpdatedAt').getUpdatedAt)
-          .to.be.at.most(results.lastModified);
-        done(error, results);
-      });
-
+    Child.get(options, (error, results) => {
+      expect(error).to.not.exist;
+      expect(results).to.exist;
+      expect(results.data).to.exist;
+      expect(results.data).to.have.length(20);
+      expect(results.total).to.exist;
+      expect(results.total).to.be.equal(32);
+      expect(results.limit).to.exist;
+      expect(results.limit).to.be.equal(20);
+      expect(results.skip).to.exist;
+      expect(results.skip).to.be.equal(0);
+      expect(results.page).to.exist;
+      expect(results.page).to.be.equal(1);
+      expect(results.pages).to.exist;
+      expect(results.pages).to.be.equal(2);
+      expect(results.lastModified).to.exist;
+      expect(_.maxBy(results.data, 'updatedAt').updatedAt)
+        .to.be.at.most(results.lastModified);
+      done(error, results);
+    });
   });
 
-
-  it('should be able to search with options', (done) => {
-
-    const options = { filter: { q: father.name } };
-    User
-      .get(options, (error, results) => {
-        expect(error).to.not.exist;
-        expect(results).to.exist;
-        expect(results.data).to.exist;
-        expect(results.data).to.have.length.at.least(1);
-        expect(results.total).to.exist;
-        expect(results.total).to.be.at.least(1);
-        expect(results.limit).to.exist;
-        expect(results.limit).to.be.equal(10);
-        expect(results.skip).to.exist;
-        expect(results.skip).to.be.equal(0);
-        expect(results.page).to.exist;
-        expect(results.page).to.be.equal(1);
-        expect(results.pages).to.exist;
-        expect(results.pages).to.be.equal(1);
-        expect(results.lastModified).to.exist;
-        expect(_.maxBy(results.data, 'getUpdatedAt').getUpdatedAt)
-          .to.be.at.most(results.lastModified);
-        done(error, results);
-      });
-
+  it('should work with search using `get` static method', done => {
+    const options = { filter: { q: children[0].name } };
+    Child.get(options, (error, results) => {
+      expect(error).to.not.exist;
+      expect(results).to.exist;
+      expect(results.data).to.exist;
+      expect(results.data).to.have.length.at.least(1);
+      expect(results.total).to.exist;
+      expect(results.total).to.be.at.least(1);
+      expect(results.limit).to.exist;
+      expect(results.limit).to.be.equal(10);
+      expect(results.skip).to.exist;
+      expect(results.skip).to.be.equal(0);
+      expect(results.page).to.exist;
+      expect(results.page).to.be.equal(1);
+      expect(results.pages).to.exist;
+      expect(results.pages).to.be.equal(1);
+      expect(results.lastModified).to.exist;
+      expect(_.maxBy(results.data, 'updatedAt').updatedAt)
+        .to.be.at.most(results.lastModified);
+      done(error, results);
+    });
   });
 
-
-  it('should parse filter options', (done) => {
-    const options = { filter: { age: 10 } };
-    User
-      .get(options, (error, results) => {
-        expect(error).to.not.exist;
-        expect(results).to.exist;
-        expect(results.data).to.exist;
-        expect(results.data).to.have.length(1);
-        expect(results.total).to.exist;
-        expect(results.total).to.be.equal(1);
-        expect(results.limit).to.exist;
-        expect(results.limit).to.be.equal(10);
-        expect(results.skip).to.exist;
-        expect(results.skip).to.be.equal(0);
-        expect(results.page).to.exist;
-        expect(results.page).to.be.equal(1);
-        expect(results.pages).to.exist;
-        expect(results.pages).to.be.equal(1);
-        expect(results.lastModified).to.exist;
-        expect(_.maxBy(results.data, 'getUpdatedAt').getUpdatedAt)
-          .to.be.at.most(results.lastModified);
-        done(error, results);
-      });
-
+  it('should work with filter using `get` static method', done => {
+    const options = { filter: { age: children[0].age } };
+    Child.get(options, (error, results) => {
+      expect(error).to.not.exist;
+      expect(results).to.exist;
+      expect(results.data).to.exist;
+      expect(results.data).to.have.length(1);
+      expect(results.total).to.exist;
+      expect(results.total).to.be.equal(1);
+      expect(results.limit).to.exist;
+      expect(results.limit).to.be.equal(10);
+      expect(results.skip).to.exist;
+      expect(results.skip).to.be.equal(0);
+      expect(results.page).to.exist;
+      expect(results.page).to.be.equal(1);
+      expect(results.pages).to.exist;
+      expect(results.pages).to.be.equal(1);
+      expect(results.lastModified).to.exist;
+      expect(_.maxBy(results.data, 'updatedAt').updatedAt)
+        .to.be.at.most(results.lastModified);
+      done(error, results);
+    });
   });
 
-  it('should be able to search and filter', (done) => {
-
-    const options = { filter: { q: father.name, age: { $eq: father.age } } };
-    User
-      .get(options, (error, results) => {
-        expect(error).to.not.exist;
-        expect(results).to.exist;
-        expect(results.data).to.exist;
-        expect(results.data).to.have.length.at.least(1);
-        expect(results.total).to.exist;
-        expect(results.total).to.be.at.least(1);
-        expect(results.limit).to.exist;
-        expect(results.limit).to.be.equal(10);
-        expect(results.skip).to.exist;
-        expect(results.skip).to.be.equal(0);
-        expect(results.page).to.exist;
-        expect(results.page).to.be.equal(1);
-        expect(results.pages).to.exist;
-        expect(results.pages).to.be.equal(1);
-        expect(results.lastModified).to.exist;
-        expect(_.maxBy(results.data, 'getUpdatedAt').getUpdatedAt)
-          .to.be.at.most(results.lastModified);
-        done(error, results);
-      });
-
+  it('should work with filter and search using `get` static method', done => {
+    const options = {
+      filter: {
+        q: children[0].name,
+        age: { $eq: children[0].age }
+      }
+    };
+    Child.get(options, (error, results) => {
+      expect(error).to.not.exist;
+      expect(results).to.exist;
+      expect(results.data).to.exist;
+      expect(results.data).to.have.length.at.least(1);
+      expect(results.total).to.exist;
+      expect(results.total).to.be.at.least(1);
+      expect(results.limit).to.exist;
+      expect(results.limit).to.be.equal(10);
+      expect(results.skip).to.exist;
+      expect(results.skip).to.be.equal(0);
+      expect(results.page).to.exist;
+      expect(results.page).to.be.equal(1);
+      expect(results.pages).to.exist;
+      expect(results.pages).to.be.equal(1);
+      expect(results.lastModified).to.exist;
+      expect(_.maxBy(results.data, 'updatedAt').updatedAt)
+        .to.be.at.most(results.lastModified);
+      done(error, results);
+    });
   });
 
-  it('should throw if projection are not valid', (done) => {
+  it('should fail using `get` static method with invalid projections', done => {
     const options = { select: { name: 1, age: 0 } };
-    User
-      .get(options, (error, results) => {
-        expect(error).to.exist;
-        expect(results).to.not.exist;
-        //assert error
-        expect(error.status).to.be.equal(400);
-        expect(error.name).to.be.equal('MongoError');
-        expect(error.message)
-          .to.be.equal(
-            'Projection cannot have a mix of inclusion and exclusion.'
-          );
-        done();
-      });
-
-  });
-
-  describe('headers', () => {
-    let lastModified;
-
-    beforeEach((done) => {
-      User
-        .findOne({}, { getUpdatedAt: 1 }, { autopopulate: false })
-        .sort({ getUpdatedAt: -1 })
-        .exec((error, latest) => {
-          lastModified = latest;
-          done(error, latest);
-        });
+    Child.get(options, (error, results) => {
+      expect(error).to.exist;
+      expect(results).to.not.exist;
+      expect(error.status).to.be.equal(400);
+      expect(error.name).to.be.equal('MongoError');
+      expect(error.message)
+        .to.be.equal(
+          'Projection cannot have a mix of inclusion and exclusion.'
+        );
+      done();
     });
+  });
 
-    it('should be able to get only latest modified', (done) => {
-
-      const options =
-        ({ headers: { ifModifiedSince: lastModified.getUpdatedAt } });
-
-      User
-        .fresh(options, (error, results) => {
-          expect(error).to.not.exist;
-          expect(results).to.exist;
-          expect(results.data).to.exist;
-          expect(results.data).to.have.length(0);
-          expect(results.total).to.exist;
-          expect(results.total).to.be.equal(0);
-          expect(results.limit).to.exist;
-          expect(results.limit).to.be.equal(10);
-          expect(results.skip).to.exist;
-          expect(results.skip).to.be.equal(0);
-          expect(results.page).to.exist;
-          expect(results.page).to.be.equal(1);
-          expect(results.pages).to.exist;
-          expect(results.pages).to.be.equal(0);
-          expect(results.lastModified).to.exist;
-          expect(results.lastModified)
-            .to.be.eql(lastModified.getUpdatedAt);
-          done(error, results);
-        });
-
+  it('should work with headers using `fresh` static method', done => {
+    const lastModified = _.maxBy(children, 'updatedAt').updatedAt;
+    const options = { headers: { ifModifiedSince: lastModified } };
+    Child.fresh(options, (error, results) => {
+      expect(error).to.not.exist;
+      expect(results).to.exist;
+      expect(results.data).to.exist;
+      expect(results.data).to.have.length(0);
+      expect(results.total).to.exist;
+      expect(results.total).to.be.equal(0);
+      expect(results.limit).to.exist;
+      expect(results.limit).to.be.equal(10);
+      expect(results.skip).to.exist;
+      expect(results.skip).to.be.equal(0);
+      expect(results.page).to.exist;
+      expect(results.page).to.be.equal(1);
+      expect(results.pages).to.exist;
+      expect(results.pages).to.be.equal(0);
+      expect(results.lastModified).to.exist;
+      expect(results.lastModified).to.be.eql(lastModified);
+      done(error, results);
     });
-
   });
 
-
-  after((done) => {
-    User.deleteMany(done);
-  });
+  after(done => clear(Guardian, Child, done));
 
 });
