85b084581688ae53906a886fc745a4f1cdf94fdc
lykmapipo <lallyelias87@gmail.com>
2019-05-10 11:12:48 +0300

    feat: ignore instances on put static method

--- test/integration/put.spec.js
+++ test/integration/put.spec.js
@@ -12,7 +12,7 @@ const {
 } = require('@lykmapipo/mongoose-test-helpers');
 const actions = include(__dirname, '..', '..');
 
-describe('put', () => {
+describe.only('static put', () => {
 
   const Guardian = createTestModel({
     email: { type: String, unique: true, fake: f => f.internet.email() }
@@ -23,47 +23,50 @@ describe('put', () => {
     father: { type: ObjectId, ref: Guardian.modelName }
   }, actions);
 
-  let father = Guardian.fake();
-  let child = Child.fake();
-  child.father = father;
+  let father;
+  let child;
 
-  before(done => clear(Guardian, Child, done));
+  beforeEach(done => clear(Guardian, Child, done));
 
-  before(done => create(father, done));
+  beforeEach(done => {
+    father = Guardian.fake();
+    child = Child.fake();
+    child.father = father;
 
-  before(done => create(child, done));
+    create(father, child, (error, created) => {
+      father = _.first(created);
+      child = _.last(created);
+      done(error, created);
+    });
+  });
 
-  it('should work using `put` static method', done => {
+  it.only('should work with id and object as updates', done => {
     const updates = _.pick(Guardian.fake(), 'name');
-
     Guardian.put(father._id, updates, (error, updated) => {
       expect(error).to.not.exist;
       expect(updated).to.exist;
       expect(updated._id).to.exist.and.be.eql(father._id);
-      expect(updated.name).to.equal(updates.name);
-      expect(updated.email).to.equal(father.email);
-      expect(updated.createdAt).to.exist;
-      expect(updated.updatedAt).to.exist;
+      expect(updated.createdAt).to.exist.and.be.eql(father.createdAt);
+      expect(updated.updatedAt).to.exist.and.be.above(father.updatedAt);
+      expect(updated.name).to.be.eql(updates.name);
       done(error, updated);
     });
   });
 
-  it('should work with object using `put` static method', done => {
+  it.only('should work with object as updates', done => {
     const updates = father.fakeOnly('name').toObject();
-
     Guardian.put(updates, (error, updated) => {
       expect(error).to.not.exist;
       expect(updated).to.exist;
       expect(updated._id).to.exist.and.be.eql(father._id);
-      expect(updated.name).to.equal(updates.name);
-      expect(updated.email).to.equal(father.email);
-      expect(updated.createdAt).to.exist;
-      expect(updated.updatedAt).to.exist;
+      expect(updated.createdAt).to.exist.and.be.eql(father.createdAt);
+      expect(updated.updatedAt).to.exist.and.be.above(father.updatedAt);
+      expect(updated.name).to.be.eql(updates.name);
       done(error, updated);
     });
   });
 
-  it('should work with instance using `put` static method', done => {
+  it.only('should work with id and instance as updates', done => {
     const updates = father.fakeOnly('name');
     Guardian.put(father._id, updates, (error, updated) => {
       expect(error).to.not.exist;
@@ -77,7 +80,7 @@ describe('put', () => {
     });
   });
 
-  it('should work with instance using `put` static method', done => {
+  it('should work with instance as updates', done => {
     const updates = father.fakeOnly('name');
     Guardian.put(updates, (error, updated) => {
       expect(error).to.not.exist;
@@ -87,11 +90,12 @@ describe('put', () => {
       expect(updated.email).to.equal(father.email);
       expect(updated.createdAt).to.exist;
       expect(updated.updatedAt).to.exist;
+      father = updated;
       done(error, updated);
     });
   });
 
-  it('should work with refs using `put` static method', done => {
+  it('should work with refs when updates', done => {
     const updates = _.pick(Child.fake(), 'name');
     Child.put(child._id, updates, (error, updated) => {
       expect(error).to.not.exist;
@@ -102,11 +106,14 @@ describe('put', () => {
       expect(updated.createdAt).to.exist;
       expect(updated.updatedAt).to.exist;
       expect(updated.father).to.exist;
+      expect(updated.father.name).to.exist;
+      console.log('updated', updated);
+      child = updated;
       done(error, updated);
     });
   });
 
-  it.skip('should work with refs using `put` static method', done => {
+  it('should work with refs using `put` static method', done => {
     const updates = Child.fake('name');
     Child.put(child._id, updates, (error, updated) => {
       expect(error).to.not.exist;
@@ -120,40 +127,7 @@ describe('put', () => {
       expect(updated.father._id).to.exist.and.be.eql(father._id);
       expect(updated.father.name).to.equal(father.name);
       expect(updated.father.email).to.equal(father.email);
-      done(error, updated);
-    });
-  });
-
-  it('should work using `put` instance method', done => {
-    const updates = _.pick(Guardian.fake(), 'name');
-
-    father.put(updates, (error, updated) => {
-      expect(error).to.not.exist;
-      expect(updated).to.exist;
-      expect(updated._id).to.exist.and.be.eql(father._id);
-      expect(updated.name).to.equal(updates.name);
-      expect(updated.email).to.equal(father.email);
-      expect(updated.createdAt).to.exist;
-      expect(updated.updatedAt).to.exist;
-      done(error, updated);
-    });
-  });
-
-  it('should work with refs using `put` instance method', done => {
-    const updates = _.pick(Child.fake(), 'name');
-
-    child.put(updates, (error, updated) => {
-      expect(error).to.not.exist;
-      expect(updated).to.exist;
-      expect(updated._id).to.exist.and.be.eql(child._id);
-      expect(updated.name).to.equal(updates.name);
-      expect(updated.email).to.equal(child.email);
-      expect(updated.createdAt).to.exist;
-      expect(updated.updatedAt).to.exist;
-      expect(updated.father).to.exist;
-      expect(updated.father._id).to.exist.and.be.eql(father._id);
-      expect(updated.father.name).to.equal(father.name);
-      expect(updated.father.email).to.equal(father.email);
+      child = updated;
       done(error, updated);
     });
   });
