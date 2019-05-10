cb7e5106046ae3c900eff8f2ff515d068764e068
lykmapipo <lallyelias87@gmail.com>
2019-05-10 12:24:18 +0300

    refactor(put): honour instances as updates

--- test/integration/put.spec.js
+++ test/integration/put.spec.js
@@ -153,7 +153,34 @@ describe.only('put', () => {
   // 
   // instances
   // 
-  
+  it('should work on instance with updates', done => {
+    const updates = _.pick(Guardian.fake(), 'name');
+    father.put(updates, (error, updated) => {
+      expect(error).to.not.exist;
+      expect(updated).to.exist;
+      expect(updated._id).to.exist.and.be.eql(father._id);
+      expect(updated.createdAt).to.exist.and.be.eql(father.createdAt);
+      // expect(updated.updatedAt).to.exist.and.be.above(father.updatedAt);
+      expect(updated.name).to.be.eql(updates.name);
+      done(error, updated);
+    });
+  });
+
+  it('should work on instance with refs with updates', done => {
+    const updates = _.pick(Child.fake(), 'name');
+    child.put(updates, (error, updated) => {
+      expect(error).to.not.exist;
+      expect(updated).to.exist;
+      expect(updated._id).to.exist.and.be.eql(child._id);
+      expect(updated.createdAt).to.exist.and.be.eql(child.createdAt);
+      // expect(updated.updatedAt).to.exist.and.be.above(child.updatedAt);
+      expect(updated.name).to.be.eql(updates.name);
+      expect(updated.father).to.exist;
+      expect(updated.father._id).to.exist.and.be.eql(father._id);
+      expect(updated.father.name).to.equal(father.name);
+      done(error, updated);
+    });
+  });
 
   after(done => clear(Guardian, Child, done));
 
