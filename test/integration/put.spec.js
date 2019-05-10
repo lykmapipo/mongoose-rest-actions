5a6453831b6b15828ac901bb92ffc2a353b60b83
lykmapipo <lallyelias87@gmail.com>
2019-05-10 12:18:43 +0300

    feat: ignore _id & updatedAt updates on instance.put method

--- test/integration/put.spec.js
+++ test/integration/put.spec.js
@@ -12,7 +12,7 @@ const {
 } = require('@lykmapipo/mongoose-test-helpers');
 const actions = include(__dirname, '..', '..');
 
-describe.only('static put', () => {
+describe.only('put', () => {
 
   const Guardian = createTestModel({
     email: { type: String, unique: true, fake: f => f.internet.email() }
@@ -37,6 +37,18 @@ describe.only('static put', () => {
       father = _.first(created);
       child = _.last(created);
       done(error, created);
+    });
+  });
+
+  //
+  // statics
+  //
+  it('should fail if no updated document exists', done => {
+    const fake = Guardian.fake();
+    Guardian.put(fake._id, {}, error => {
+      expect(error).to.exist;
+      expect(error.name).to.eql('DocumentNotFoundError');
+      done();
     });
   });
 
@@ -73,61 +85,75 @@ describe.only('static put', () => {
       expect(updated).to.exist;
       expect(updated._id).to.exist.and.be.eql(father._id);
       expect(updated.createdAt).to.exist.and.be.eql(father.createdAt);
-      expect(updated.updatedAt).to.exist.and.be.above(father.updatedAt);
+      // expect(updated.updatedAt).to.exist.and.be.above(father.updatedAt);
       expect(updated.name).to.be.eql(updates.name);
       done(error, updated);
     });
   });
 
-  it.only('should work with instance as updates', done => {
+  it('should work with instance as updates', done => {
     const updates = father.fakeOnly('name');
     Guardian.put(updates, (error, updated) => {
       expect(error).to.not.exist;
       expect(updated).to.exist;
       expect(updated._id).to.exist.and.be.eql(father._id);
       expect(updated.createdAt).to.exist.and.be.eql(father.createdAt);
-      expect(updated.updatedAt).to.exist.and.be.above(father.updatedAt);
+      // expect(updated.updatedAt).to.exist.and.be.above(father.updatedAt);
       expect(updated.name).to.be.eql(updates.name);
       done(error, updated);
     });
   });
 
-  it('should work with refs when updates', done => {
+  it('should work with non populated refs when updates', done => {
     const updates = _.pick(Child.fake(), 'name');
     Child.put(child._id, updates, (error, updated) => {
       expect(error).to.not.exist;
       expect(updated).to.exist;
       expect(updated._id).to.exist.and.be.eql(child._id);
-      expect(updated.name).to.equal(updates.name);
-      expect(updated.email).to.equal(child.email);
-      expect(updated.createdAt).to.exist;
-      expect(updated.updatedAt).to.exist;
+      expect(updated.createdAt).to.exist.and.be.eql(child.createdAt);
+      expect(updated.updatedAt).to.exist.and.be.above(child.updatedAt);
+      expect(updated.name).to.be.eql(updates.name);
       expect(updated.father).to.exist;
-      expect(updated.father.name).to.exist;
-      console.log('updated', updated);
-      child = updated;
       done(error, updated);
     });
   });
 
-  it('should work with refs using `put` static method', done => {
-    const updates = Child.fake('name');
+  it('should work with populated refs when updates', done => {
+    const updates = child.fakeOnly('name');
     Child.put(child._id, updates, (error, updated) => {
       expect(error).to.not.exist;
       expect(updated).to.exist;
       expect(updated._id).to.exist.and.be.eql(child._id);
-      expect(updated.name).to.equal(updates.name);
-      expect(updated.email).to.equal(child.email);
-      expect(updated.createdAt).to.exist;
-      expect(updated.updatedAt).to.exist;
+      expect(updated.createdAt).to.exist.and.be.eql(child.createdAt);
+      // expect(updated.updatedAt).to.exist.and.be.above(child.updatedAt);
+      expect(updated.name).to.be.eql(updates.name);
       expect(updated.father).to.exist;
       expect(updated.father._id).to.exist.and.be.eql(father._id);
       expect(updated.father.name).to.equal(father.name);
-      expect(updated.father.email).to.equal(father.email);
-      child = updated;
       done(error, updated);
     });
   });
+
+  it('should work with populated refs when updates', done => {
+    const updates = child.fakeOnly('name');
+    Child.put(updates, (error, updated) => {
+      expect(error).to.not.exist;
+      expect(updated).to.exist;
+      expect(updated._id).to.exist.and.be.eql(child._id);
+      expect(updated.createdAt).to.exist.and.be.eql(child.createdAt);
+      // expect(updated.updatedAt).to.exist.and.be.above(child.updatedAt);
+      expect(updated.name).to.be.eql(updates.name);
+      expect(updated.father).to.exist;
+      expect(updated.father._id).to.exist.and.be.eql(father._id);
+      expect(updated.father.name).to.equal(father.name);
+      done(error, updated);
+    });
+  });
+
+  // 
+  // instances
+  // 
+  
 
   after(done => clear(Guardian, Child, done));
 
