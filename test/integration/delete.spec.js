9ff300dbc9a1abbfe4de119ca78e52bedf38ea41
lykmapipo <lallyelias87@gmail.com>
2019-05-20 15:03:33 +0300

    test: debug and test delete with filter options

--- test/integration/delete.spec.js
+++ test/integration/delete.spec.js
@@ -17,11 +17,12 @@ describe('delete', () => {
   let father = User.fake();
   let mother = User.fake();
   let aunt = User.fake();
+  let brother = User.fake();
   let child = User.fake();
 
   before(done => clear(User, done));
 
-  before(done => create(father, mother, aunt, child, done));
+  before(done => create(father, mother, aunt, brother, child, done));
 
   it('should work using `del` static method', done => {
     User.del(father._id, (error, deleted) => {
@@ -74,6 +75,16 @@ describe('delete', () => {
       expect(deleted).to.exist;
       expect(deleted._id).to.eql(aunt._id);
       expect(deleted.deletedAt).to.exist;
+      done(error, deleted);
+    });
+  });
+
+  it('should delete using `del` static method with filter', done => {
+    const options = { _id: brother._id, filter: { name: brother.name } };
+    User.del(options, (error, deleted) => {
+      expect(error).to.not.exist;
+      expect(deleted).to.exist;
+      expect(deleted._id).to.eql(brother._id);
       done(error, deleted);
     });
   });
