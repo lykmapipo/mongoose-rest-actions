d099ee1e5e2f63af0f80ce6a4ecadb32b3320ed9
lykmapipo <lallyelias87@gmail.com>
2019-05-20 14:54:34 +0300

    feat(delete): add support to filter

--- test/unit/delete.spec.js
+++ test/unit/delete.spec.js
@@ -50,6 +50,45 @@ describe('delete', () => {
     });
   });
 
+  it('should work using `del` static method with filter', done => {
+
+    const User = createTestModel({}, schema => {
+      schema.methods.beforeDelete = done => done();
+      schema.methods.afterDelete = done => done();
+    }, del);
+    const user = User.fake();
+
+    const Mock = mockModel(User);
+    const mock = mockInstance(user);
+
+    const findById = Mock.expects('findById');
+    const where = findById.chain('where');
+    const exec = findById.chain('exec').yields(null, user);
+    const remove = mock.expects('remove').yields(null, user);
+    const beforeDelete = sinon.spy(user, 'beforeDelete');
+    const afterDelete = sinon.spy(user, 'afterDelete');
+
+    const options = { _id: user._id, filter: { name: user.name } };
+    User.del(options, (error, deleted) => {
+      Mock.verify();
+      Mock.restore();
+
+      mock.verify();
+      mock.restore();
+
+      expect(findById).to.have.been.calledOnce;
+      expect(findById).to.have.been.calledWith(user._id);
+      expect(where).to.have.been.calledOnce;
+      expect(where).to.have.been.calledWith(options.filter);
+      expect(exec).to.have.been.calledOnce;
+      expect(remove).to.have.been.calledOnce;
+      expect(beforeDelete).to.have.been.calledOnce;
+      expect(afterDelete).to.have.been.calledOnce;
+
+      done(error, deleted);
+    });
+  });
+
   it('should work using `del` instance method', done => {
 
     const User = createTestModel({}, schema => {
