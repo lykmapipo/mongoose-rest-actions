a2eb7e6b8092ae771ec5880bfcbe5ebc27a4607c
lykmapipo <lallyelias87@gmail.com>
2019-05-20 15:35:30 +0300

    test: debug and test patch with filter options

--- test/integration/patch.spec.js
+++ test/integration/patch.spec.js
@@ -24,17 +24,20 @@ describe('patch', () => {
   }, actions);
 
   let father;
+  let aunt;
   let child;
 
   beforeEach(done => clear(Guardian, Child, done));
 
   beforeEach(done => {
     father = Guardian.fake();
+    aunt = Guardian.fake();
     child = Child.fake();
     child.father = father;
 
-    create(father, child, (error, created) => {
+    create(father, aunt, child, (error, created) => {
       father = _.first(created);
+      aunt = _.nth(created, 1);
       child = _.last(created);
       done(error, created);
     });
@@ -147,6 +150,20 @@ describe('patch', () => {
       expect(updated.father).to.exist;
       expect(updated.father._id).to.exist.and.be.eql(father._id);
       expect(updated.father.name).to.equal(father.name);
+      done(error, updated);
+    });
+  });
+
+  it('should work with object as updates and options with filter', done => {
+    const options = { _id: aunt._id, filter: { name: aunt.name } };
+    const updates = _.pick(aunt.fakeOnly('name'), 'name');
+    Guardian.patch(options, updates, (error, updated) => {
+      expect(error).to.not.exist;
+      expect(updated).to.exist;
+      expect(updated._id).to.exist.and.be.eql(aunt._id);
+      expect(updated.createdAt).to.exist.and.be.eql(aunt.createdAt);
+      expect(updated.updatedAt).to.exist.and.be.above(aunt.updatedAt);
+      expect(updated.name).to.be.eql(updates.name);
       done(error, updated);
     });
   });
--- test/unit/patch.spec.js
+++ test/unit/patch.spec.js
@@ -52,6 +52,46 @@ describe('patch', () => {
     });
   });
 
+  it('should work using `patch` static method with filter', done => {
+
+    const User = createTestModel({}, schema => {
+      schema.methods.beforePatch = (updates, done) => done();
+      schema.methods.afterPatch = (updates, done) => done();
+    }, patch);
+    const user = User.fake();
+
+    const Mock = mockModel(User);
+    const mock = mockInstance(user);
+
+    const findById = Mock.expects('findById');
+    const where = findById.chain('where');
+    const exec = findById.chain('exec').yields(null, user);
+    const save = mock.expects('save').yields(null, user);
+    const beforePatch = sinon.spy(user, 'beforePatch');
+    const afterPatch = sinon.spy(user, 'afterPatch');
+
+    const updates = _.pick(User.fake(), 'name');
+    const options = { _id: user._id, filter: { name: user.name } };
+    User.patch(options, updates, (error, updated) => {
+      Mock.verify();
+      Mock.restore();
+
+      mock.verify();
+      mock.restore();
+
+      expect(findById).to.have.been.calledOnce;
+      expect(findById).to.have.been.calledWith(user._id);
+      expect(where).to.have.been.calledOnce;
+      expect(where).to.have.been.calledWith(options.filter);
+      expect(exec).to.have.been.calledOnce;
+      expect(save).to.have.been.calledOnce;
+      expect(beforePatch).to.have.been.calledOnce;
+      expect(afterPatch).to.have.been.calledOnce;
+
+      done(error, updated);
+    });
+  });
+
   it('should work using `patch` instance method', done => {
 
     const User = createTestModel({}, schema => {
